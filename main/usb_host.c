94416771f8fec0f1f9e9730672d75e1684b23952
TechnoWolves <kaygaz32@gmail.com>
2025-09-01 19:32:35 +0300

    Update usb_host.c

--- main/usb_host.c
+++ main/usb_host.c
@@ -1,12 +1,19 @@
+#include "usb_host.h"
 #include "tusb.h"
 #include "ble_mouse.h"
 #include "esp_log.h"
 
 static const char *TAG = "USB_HOST";
 
+void usb_host_init(void)
+{
+    ESP_LOGI(TAG, "TinyUSB başlatılıyor...");
+    tuh_init();
+}
+
 void usb_mouse_poll(void)
 {
-    tuh_task(); // USB host stack
+    tuh_task();
 
     for (uint8_t dev_addr = 1; dev_addr <= CFG_TUH_DEVICE_MAX; dev_addr++)
     {
@@ -17,9 +24,8 @@ void usb_mouse_poll(void)
 
         if (tuh_hid_mouse_read(dev_addr, &buttons, &x, &y, &wheel))
         {
-            // BLE Mouse hareketlerini gönder
-            ble_mouse_move(x, y, wheel);
             ble_mouse_buttons(buttons);
+            ble_mouse_move(x, y, wheel);
         }
     }
 }
