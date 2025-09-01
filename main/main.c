a5f6a18b2ec0366945c11bc15a9fb5a5ade46400
TechnoWolves <kaygaz32@gmail.com>
2025-09-01 19:29:09 +0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,33 +1,26 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-#include "esp_err.h"
-
 #include "usb_host.h"
-#include "tinyusb_host.h"
 #include "ble_mouse.h"
 
-static const char *TAG = "USB2BT_MOUSE";
+static const char *TAG = "BedwarsMacro";
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "Başlatılıyor...");
 
-    // 1. USB Host başlat
+    // USB Host başlat
     ESP_LOGI(TAG, "USB Host başlatılıyor...");
-    ESP_ERROR_CHECK(tuh_init());
+    usb_host_init();
 
-    // 2. BLE Mouse başlat
+    // BLE Mouse başlat
     ESP_LOGI(TAG, "BLE Mouse başlatılıyor...");
     ble_mouse_init();
 
     while(1) {
-        // USB Task
-        tuh_task();
-
-        // USB fare raporlarını oku
+        // USB fareyi oku ve BLE üzerinden gönder
         usb_mouse_poll();
-
         vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
